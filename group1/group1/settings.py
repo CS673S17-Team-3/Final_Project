5b1d9cff34871d12d1f8492c5a57baa70e8f06f5
Ashutosh Singh <ashsingh@bu.edu>
2017-02-20 23:42:18 -0500

    fixed csrf error and file upload path fixed

--- group1/group1/settings.py
+++ group1/group1/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'group1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'NAME': 'c:\\Users\\ashu\\Final_Project\\database\database.sqlite3',
     }
 }
 
