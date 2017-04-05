9c5781cf5885fc5553cdb70faf99eebacbabe056
sonmn <sonmn@bu.edu>
2017-04-05 14:13:25 -0700

    create an automate script to gather metrics

---
+++ group1/createMetric.sh
@@ -0,0 +1,17 @@
+
+echo "comm: \n"
+cloc comm/
+echo "communication folder(ignore node modules): \n"
+cloc communication/django
+echo "group 1 folder: \n"
+cloc group1/
+echo "selenium tests folder: \n"
+cloc selenium_tests/
+echo "unit tests folder: \n"
+cloc unit_tests/
+echo "project_router folder: \n"
+cloc project_router/
+
+
+cloc manage.py
+cloc settings.html
